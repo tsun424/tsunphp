aef3eeced7b40fee4fcd62714a8079d0ee3f7945
tsun424 <tsun424@163.com>
2015-10-13 22:54:45 +1300

    1014

--- index.php
+++ index.php
@@ -3,5 +3,4 @@
 	define("BASE_PATH",__DIR__);					//define the entrance of the framework
 	define("APP_PATH",BASE_PATH."/home");				//define the home directory of the application
 	require(BASE_PATH."/tsun/tsun.php");
-	//require(BASE_PATH."/tsun/test1.php");
 ?>
